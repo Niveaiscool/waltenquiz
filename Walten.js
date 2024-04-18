83524b250f0c0eb3688fa4b4349d70e012777570
Niveaiscool <143618734+Niveaiscool@users.noreply.github.com>
2024-04-18 14:10:28 +0000

    fin

---
+++ Walten.js
@@ -0,0 +1,24 @@
+let button= document.querySelector("button");
+let button2= document.querySelector("button");
+
+let input1 = document.querySelector(".age");
+let input2 = document.querySelector(".yn");
+let input3 = document.querySelector(".alone");
+
+button.onclick = function() {
+    console.log("button check");
+
+    let answer1 = Number(input1.value);
+console.log("answer one");
+
+
+};
+
+button.onclick = function() {
+    console.log("button check");
+
+    let answer2 = input2.value;
+    console.log(answer2);
+    
+
+};
